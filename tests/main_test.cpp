c2d26dfef5b70b4912ab29a64dbcb82ba77d6b70
mp <max.pfeifer@physik.hu-berlin.de>
2021-03-22 21:31:06 +0100

    [cmake] get auto compilation of cpp in tests folder working with cmake

---
+++ tests/main_test.cpp
@@ -0,0 +1,8 @@
+#define BOOST_TEST_MODULE MMie test suite
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+
+
+
+
---
+++ tests/test_FFNet.cpp
@@ -0,0 +1,8 @@
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_CASE( FFNeuralNet, * boost::unit_test::tolerance(1e-15) ){
+  
+}
